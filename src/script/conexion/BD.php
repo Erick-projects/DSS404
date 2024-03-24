85c28e975b90967c56d0e04c871efdfe08b497cf
Julian Moreno <juliancastillo204@gmail.com>
2024-03-23 20:23:33 -0600

    Formulario Usuario, Formulario creacion de usuario

--- src/script/conexion/BD.php
+++ src/script/conexion/BD.php
@@ -1,17 +1,18 @@
 <?php
 //Datos de conexión
-define("DBHOST", "localhost"); //CAMBIAR PUERTO SI ES NECESARIO
-define("DBUSER", "root");
-define("DBPASS", "1234");  //EN MI CASO SI USO CONTRA USTEDES CAMBIENLA
-define("DBDATA", "banco"); //Si le cambian el nombre a la bd, lo cambian aqui 
+$servername = "localhost";
+$username = "root";
+$password = "1234";//CAMBIAR CONTRA IMPORTANTE
+$dbname = "banco";
 
-//Creando el objeto de conexión a la base de datos con MySQLi
-$db = new mysqli(DBHOST, DBUSER, DBPASS, DBDATA);
-//Verificar que la conexión se ha realizado o terminar
-//el programa o secuencia de comando si no ha sido así
-if($db->connect_errno){
- die("No se ha podido conectar a MySQL: (" . $db->connect_errno . ")" . $db->connect_error);
+// Conectar a la BD
+$db = mysqli_connect($servername, $username, $password, $dbname);
+
+// Verificar si la conexión falló
+if (!$db) {
+    die("Error de conexión: " . mysqli_connect_error());
 }
+
 //Establecer el conjunto de caracteres para no tener problemas
 //con los caracteres especiales del idioma español
 $db->set_charset("utf8");
--- src/script/validacionUsuario.php
+++ src/script/validacionUsuario.php
@@ -9,29 +9,20 @@
         $email = $_POST['email'];
     
     // Consulta SQL para verificar las credenciales  
-    $sql = "SELECT * FROM usuario WHERE nombreUsuario=? AND contraseña=? AND Email=?";
-     
-    // Preparar la consulta
-    $stmt = mysqli_prepare($db, $sql);
-    
-    // Vincular los parámetros
-    mysqli_stmt_bind_param($stmt, "sss", $usuario, $contraseña, $email);
-
-    // Ejecutar la consulta
-    mysqli_stmt_execute($stmt);     
-
-   // Obtener el resultado
-   $resultado = mysqli_stmt_get_result($stmt);
+    $sql = "SELECT * FROM usuario WHERE nombreUsuario = '$usuario' AND contraseña = '$contraseña' AND Email = '$email'";
+    $result = mysqli_query($db, $sql);
 
     // Verificar si se encontró un registro 
-    if(mysqli_num_rows($resultado) == 1) {
+    if(mysqli_num_rows($result) >= 1) {
          // Redirigir a la página principal
     header("Location: ../html/index.html");
-       exit;
+    
     } else {
      // Credenciales incorrectas
       header("Location: ../html/formulario.html");
       exit;
      }
 }    
+
+mysqli_close($db);
 ?>
